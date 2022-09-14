9945fb905d6688ec2a3444e1e82409af3b5fa1fb
manise <m.heidarizojabad@student.fontys.nl>
2022-09-14 09:39:34 +0200

    Update 14 September

---
+++ src/EAfspraak.Entities/VerwijsBrief.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAfspraak.Entities
+{
+    public enum BriefStatus
+    {
+        Active,
+        Reserve,
+        Doen,
+        Expierd
+
+    }
+    public class VerwijsBrief
+    {
+        public int Id { get; set; } 
+        public int BehandelingId { get; set; }
+        public int PatiëntId { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public BriefStatus Status { get; set; }
+
+    }
+}
