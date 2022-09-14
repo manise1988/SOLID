4df0b22fa44e36ac5e2f1774940369b7983dc304
manise <m.heidarizojabad@student.fontys.nl>
2022-09-14 08:47:11 +0200

    update Entities

--- src/EAfspraak.Entities/BehandelingCentrum.cs
+++ src/EAfspraak.Entities/BehandelingCentrum.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace EAfspraak.Entities
 {
-    public class Behandeling
+    public class BehandelingCentrum
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-
-     
+        public string Name { get; set; }    
+        public int BehandelingId { get; set; }
     }
 }
