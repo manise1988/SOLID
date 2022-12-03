99077806bc9931c9bf33b5a3461d14f844c89d5e
manise <m.heidarizojabad@student.fontys.nl>
2022-12-03 21:07:20 +0100

    03-12 update

--- src/UnitTestProject/UnitTestAfspraak.cs
+++ src/UnitTestProject/UnitTestAfspraak.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject
     public class UnitTestAfspraak
     {
         [Fact]
-        public void TestMaakAfspraakInEenKliniek()
+        public void TestMaakAfspraakInEenKliniekAccesstrue()
         {
 
             ZoekBereik zoekBereik = new ZoekBereik(7);
